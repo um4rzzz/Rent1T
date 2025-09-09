8a417da8c28201f6b91dac7a00f924247f34d59c
um4rzzz <umar10q@gmail.com>
2025-09-09 11:04:03 +1000

    scroll bar fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="bg-[var(--bg)]">
       <Head>
-        <meta name="theme-color" content="#16A34A" media="(prefers-color-scheme: light)" />
-        <meta name="theme-color" content="#EF4444" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#0B0B0C" media="(prefers-color-scheme: dark)" />
       </Head>
       <body className="antialiased bg-[var(--bg)] text-[var(--text)]">
         <Main />
